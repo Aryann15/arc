8587d16e9d35a2afcef230657a7536fd967df2f5
Aryann15 <rajlysm15@gmail.com>
2024-08-17 00:09:20 +0530

    added input button

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,18 +1,27 @@
-import { GraphTab } from "../components/Graphtab"
+// import { GraphTab } from "../components/Graphtab"
 import { TotalExpense } from "../components/TotalExpense"
 import { BillAnalyser } from "../components/BillAnalyser"
 import { ExpensesTab } from "../components/ExpensesTab"
 
 export const Home = () => {
+    const handleFileUpload = (e) => {
+      const file = e.target.files[0]
+      if (file) {
+        console.log(file)
+      }
+  }
   return (
     <div className="container mx-auto p-4">
       <div className="grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-3 text-white text-center ">
-        <GraphTab />
+        {/* <GraphTab /> */}
         <TotalExpense />
         <BillAnalyser />
       </div>
       <div className="m-4">
         <ExpensesTab />
+      </div>
+      <div>
+        <input type="file" accept="image/*" onChange={handleFileUpload} />
       </div>
     </div>
   )
