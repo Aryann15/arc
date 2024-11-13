21f19ec3ad2849d1be484a847a56f1e2a61cf335
aryann15 <rajlysm15@gmail.com>
2024-11-13 06:34:30 +0530

    get unique categories

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -18,5 +18,6 @@ export default function TransactionPage() {
     
         fetchTransactions()
       }, [])
-    
+      const categories = ['all', ...new Set(transactions.map(t => t.category?.name || 'Uncategorized'))]
+
 }
