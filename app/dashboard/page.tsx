1de922bd08ac876a6b626654b4fd21bc5815aaf8
aryann15 <rajlysm15@gmail.com>
2024-11-13 06:10:07 +0530

    type definitions for out data

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,20 @@
 "use client";
 import React, { useRef, useState, useEffect } from "react";
 import { uploadBill } from "../actions/bill";
+
+type Bill = {
+  id: string;
+  amount: number;
+  description: string;
+  createdAt: string;
+  category?: {
+    name: string;
+  };
+};
+
+
+export default function Dashboard() {
+return (
+    <div className="p-8 max-w-7xl mx-auto">
+      </div>
+)}
