97086aff797d9bf68188f4319fe7fc6b358b83d6
aryann15 <rajlysm15@gmail.com>
2024-11-13 06:34:03 +0530

    fetch transactions

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -2,4 +2,21 @@
 import React, { useState, useEffect } from 'react'
 export default function TransactionPage() {
 
+    useEffect(() => {
+        const fetchTransactions = async () => {
+          try {
+            const response = await fetch('/api/bills')
+            const data = await response.json()
+            setTransactions(data) //tbd
+            setFilteredTransactions(data) //tbd
+            setIsLoading(false) //tbd
+          } catch (error) {
+            console.error('Error fetching transactions:', error)
+            setIsLoading(false) //tbd
+          }
+        }
+    
+        fetchTransactions()
+      }, [])
+    
 }
